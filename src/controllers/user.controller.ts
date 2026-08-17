0841ba78cb55b1b520a68b57158ca122d9e034b1
Madhuboni Bhattacharya <madhubonibhattacharya03@gmail.com>
2026-08-17 18:10:34 +0530

    error handling inside user.controller.ts for unauthorised user .

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { UserService } from "../services/user.service.js";
 import { sendSuccess } from "../utils/helpers/response.helper.js";
+import { UnauthorizedError } from "../utils/errors/app.error.js";
 
 export class UserController {
   private readonly userService: UserService;
@@ -16,8 +17,12 @@ export class UserController {
 
   async updateUserHandler(req: Request, res: Response, next: NextFunction) {
     try {
-      const loggedInUserId = req.user!.id;
-      const targetId = req.params.id;
+      if (!req.user) {
+        throw new UnauthorizedError("Please sign in first");
+      }
+
+      const loggedInUserId = req.user.id;
+      const targetId = req.params.id as string;
 
       const updatedUser = await this.userService.updateUser(loggedInUserId, targetId, req.body);
 
@@ -25,5 +30,5 @@ export class UserController {
     } catch (error) {
       next(error);
     }
-  }
+}
 }
