5a287557c619d750d4b4a7ae7738e5bd1ef04a87
shreeya-07 <shreeyachattopadhyay@gmail.com>
2026-08-18 19:34:52 +0530

    task created api 1

--- src/routes/v1/task.route.ts
+++ src/routes/v1/task.route.ts
@@ -3,6 +3,8 @@ import { TaskController } from "../../controllers/task.controller.js";
 import { TaskService } from "../../services/task.service.js";
 import { TaskRepository } from "../../repositories/task.repository.js";
 import { authenticateUser } from "../../middlewares/authentication.middleware.js";
+import { authorizeUser } from "../../middlewares/authorization.middleware.js";
+import { RoleName } from "../../types/role.type.js";
 
 import {
   validateRequestParams,
@@ -28,6 +30,7 @@ taskRouter.get(
 taskRouter.post(
   "/",
   authenticateUser,
+  authorizeUser(RoleName.ADMIN),
   validateRequestBody(createTaskSchema),
   taskController.createTaskHandler.bind(taskController)
 );
