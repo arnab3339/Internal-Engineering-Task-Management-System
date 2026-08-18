5b4dabd4a99aa482acf2680fab86b17721af3248
Arnab <arnab3339@gmail.com>
2026-08-18 14:17:40 +0530

    Add create project API

---
+++ src/routes/v1/project.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { ProjectController } from "../../controllers/project.controller.js";
+import { ProjectService } from "../../services/project.service.js";
+import { ProjectRepository } from "../../repositories/project.repository.js";
+import { authenticateUser } from "../../middlewares/authentication.middleware.js";
+import { authorizeUser } from "../../middlewares/authorization.middleware.js";
+import { RoleName } from "../../types/role.type.js";
+const router = Router();
+
+const projectRepository = new ProjectRepository();
+const projectService = new ProjectService(projectRepository);
+const projectController = new ProjectController(projectService);
+
+router.post(
+  "/projects",
+  authenticateUser,
+  authorizeUser(RoleName.ADMIN),
+  projectController.createProjectHandler.bind(projectController)
+);
+
+export default router;
