6e9e442dbc640446b18c68b2f8313113511e5714
Madhuboni Bhattacharya <madhubonibhattacharya03@gmail.com>
2026-08-20 18:18:56 +0530

    [fix:] submission route authorisation added

--- src/routes/v1/submission.route.ts
+++ src/routes/v1/submission.route.ts
@@ -2,20 +2,20 @@ import { Router } from "express";
 import { SubmissionController } from "../../controllers/submission.controller.js";
 import { SubmissionService } from "../../services/submission.service.js";
 import { SubmissionRepository } from "../../repositories/submission.repository.js";
-import { TaskAssignmentRepository } from "../../repositories/taskAssignment.repository.js";
 import { TaskService } from "../../services/task.service.js";
 import { TaskRepository } from "../../repositories/task.repository.js";
 
 import { authenticateUser } from "../../middlewares/authentication.middleware.js";
+import { authorizeUser } from "../../middlewares/authorization.middleware.js";
 import { validateRequestParams } from "../../middlewares/validate.middleware.js";
 import { taskIdSchema } from "../../dtos/task.dto.js";
+import { RoleName } from "../../types/role.type.js";
 
 export const submissionRouter = Router({ mergeParams: true });
 
 export const submissionController = new SubmissionController(
     new SubmissionService(
         new SubmissionRepository(),
-        new TaskAssignmentRepository(),
         new TaskService(new TaskRepository())
     )
 );
@@ -23,8 +23,8 @@ export const submissionController = new SubmissionController(
 submissionRouter.get(
     "/",
     authenticateUser,
+    authorizeUser(RoleName.ADMIN),
     validateRequestParams(taskIdSchema),
     submissionController.getTaskSubmissionsHandler.bind(submissionController)
 );
-
 // implement all the routers below
--- src/services/submission.service.ts
+++ src/services/submission.service.ts
@@ -1,6 +1,5 @@
 import { Submission } from "../../generated/prisma/client.js";
 import { ISubmissionRepository } from "../repositories/submission.repository.js";
-import { ITaskAssignmentRepository } from "../repositories/taskAssignment.repository.js";
 import { ITaskService } from "./task.service.js";
 import { UnauthorizedError } from "../utils/errors/app.error.js";
 import { RoleName } from "../types/role.type.js";
@@ -13,17 +12,14 @@ export interface ISubmissionService {
 
 export class SubmissionService implements ISubmissionService {
     private readonly submissionRepository: ISubmissionRepository;
-    private readonly taskAssignmentRepository: ITaskAssignmentRepository;
     private readonly taskService: ITaskService;
 
     constructor(
         submissionRepository: ISubmissionRepository,
-        taskAssignmentRepository: ITaskAssignmentRepository,
         taskService: ITaskService,
         
     ) {
         this.submissionRepository = submissionRepository;
-        this.taskAssignmentRepository = taskAssignmentRepository;
         this.taskService = taskService;
         
     }
@@ -33,14 +29,10 @@ export class SubmissionService implements ISubmissionService {
     }
 
     async findTaskSubmissions(taskId: bigint, userId: bigint, role: string): Promise<Submission[]> {
-        await this.taskService.getTaskById(taskId);
+        const task = await this.taskService.getTaskById(taskId);
 
-        if (role !== RoleName.ADMIN) {
-            const currentAssignment = await this.taskAssignmentRepository.findCurrentByTaskAndDeveloper(taskId, userId);
-
-            if (!currentAssignment) {
-                throw new UnauthorizedError("You are not authorized to view submissions for this task");
-            }
+        if (role !== RoleName.ADMIN && task.createdBy !== userId) {
+             throw new UnauthorizedError("You are not authorized to view submissions for this task");
         }
 
         return this.submissionRepository.findByTaskId(taskId);
